051fef0d901a2800d016305da87a3c0fc87de2ed
Arek Korbik <arkadini@gmail.com>
2014-12-03 23:28:34 +0100

    Minor admin improvement

--- app/tasks/admin.py
+++ app/tasks/admin.py
@@ -5,6 +5,7 @@ from .models import UnlockedKey
 
 class UnlockedKeyAdmin(admin.ModelAdmin):
     readonly_fields = ('ts',)
+    list_display = ('user', 'key', 'ts')
 
 
 admin.site.register(UnlockedKey, UnlockedKeyAdmin)
